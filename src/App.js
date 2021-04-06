727ed9f76d37ac141a06ca6faf1857863a408a0d
Ayush Raj <yoayushraj99@gmail.com>
2021-04-06 09:34:33 +0530

    Circular progress component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import useFetch from './useFetch';
 import "./App.css";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App () {
   // const [global, setGlobal] = useState({});
@@ -10,7 +11,7 @@ function App () {
   return (
     <div className="App">
         <h1>COVID-19 CORONAVIRUS PANDEMIC</h1>
-        {data ? <CardList countries={data.Countries} />: <div>Loading</div>}
+        {data ? <CardList countries={data.Countries} />: <CircularProgress color="secondary" />}
     </div>
   );
 }
